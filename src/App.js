252895027a1cc46aaa9f614eae512141254ab8b1
b <b>
2021-10-07 20:38:37 +0100

    Setup: React-Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from './components/Form';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <Router>
+      <div className='App'>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/students'>Students</Link>
+            </li>
+            <li>
+              <Link to='/register'>Add a new Student</Link>
+            </li>
+          </ul>
+        </nav>
+        <Form />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
