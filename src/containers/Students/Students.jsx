76ea60dd7c073236480ff6261f73a5365dc27296
b <b>
2021-10-12 09:27:41 +0100

    Added sort functionality

--- src/containers/Students/Students.jsx
+++ src/containers/Students/Students.jsx
@@ -3,51 +3,81 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 // Data
-import StudentData from '../../studentsDataBase.json';
 import filterStudents from '../../utils/filterStudents';
 
 // component
 import Card from '../../components/Card';
-import SearchBar from '../../components/Search';
-import Announcer from '../../components/Announcer';
+import SearchBar from '../../components/SearchBar/SearchBar';
 
 // styles
-import { StudentsContainer, InfoBar } from './Students.styles';
+import { StudentsContainer, InfoBar, Test } from './Students.styles';
+import Register from '../Register/Register';
 
-const Students = ({ students }) => {
+const Students = ({ students, setStudents }) => {
   const { search } = window.location;
   const query = new URLSearchParams(search).get('s');
   const [searchQuery, setSearchQuery] = useState(query || '');
   const filteredStudents = filterStudents(students, searchQuery);
 
+  const [sortBy, setSortBy] = useState('(A-Z)');
+  const [showRegister, setShowRegister] = useState(false);
+
+  const handleSort = () => {
+    if (sortBy === '(A-Z)') {
+      setSortBy('(Z-A)');
+      filteredStudents.sort(function (a, b) {
+        var textA = a.name.toUpperCase();
+        var textB = b.name.toUpperCase();
+        return textA < textB ? -1 : textA > textB ? 1 : 0;
+      });
+    }
+    if (sortBy === '(Z-A)') {
+      setSortBy('(A-Z)');
+      filteredStudents.sort(function (a, b) {
+        var textA = a.name.toUpperCase();
+        var textB = b.name.toUpperCase();
+        return textA > textB ? -1 : textA < textB ? 1 : 0;
+      });
+    }
+  };
+
   return (
-    <StudentsContainer>
-      <h2>Students</h2>;
-      <Announcer message={`List has ${filteredStudents.length} posts`} />
-      <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-      <InfoBar>
-        <p>
-          <strong>{filteredStudents.length} Students found</strong>
-        </p>
-        <div>
-          <ul>
-            <li>
-              <Link to='/register'>Add a new Student</Link>
-            </li>
-          </ul>
-        </div>
-      </InfoBar>
-      {filteredStudents.map((student) => {
-        return (
-          <Card>
-            <Link to={`/students/${student.id}`}>
-              <h3>{student.name}</h3>
-            </Link>
-            <p>{student.id}</p>
-          </Card>
-        );
-      })}
-    </StudentsContainer>
+    <Test>
+      <StudentsContainer>
+        <h2>Students</h2>
+        <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+        <InfoBar>
+          <p>
+            <strong>{filteredStudents.length} Students found</strong>
+          </p>
+          <div>
+            <ul>
+              <li>
+                <button onClick={() => setShowRegister(true)}>
+                  Add a new Student
+                </button>
+              </li>
+            </ul>
+          </div>
+          <button className='sortBy' onClick={handleSort}>
+            Sort by: Name {sortBy}
+          </button>
+        </InfoBar>
+        {filteredStudents.map((student) => {
+          return (
+            <Card>
+              <Link to={`/students/${student.id}`}>
+                <h3>{student.name}</h3>
+              </Link>
+              <p>{student.id}</p>
+            </Card>
+          );
+        })}
+      </StudentsContainer>
+      {showRegister && (
+        <Register students={students} setStudents={setStudents} />
+      )}
+    </Test>
   );
 };
 
